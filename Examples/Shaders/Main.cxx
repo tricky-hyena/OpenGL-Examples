f4c07b4b6240b9f6ebf47a45c488ee0995db5b05
tricky-hyena <157110410+tricky-hyena@users.noreply.github.com>
2024-02-02 18:44:12 +0300

    Add working GLFW example (Shaders).

--- Examples/Shaders/Main.cxx
+++ Examples/Shaders/Main.cxx
@@ -1,8 +1,34 @@
-#include <iostream>
+#include "GLFW/glfw3.h"
 
 int main()
 {
-	std::cout << "Example 1. Shaders.\n";
+    glfwInit();
 
-	return 0;
+    /* OpenGL version: 3.3 */
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    glfwWindowHint(
+        GLFW_OPENGL_PROFILE,
+        GLFW_OPENGL_CORE_PROFILE
+    );
+
+    glfwWindowHint(
+        GLFW_OPENGL_FORWARD_COMPAT,
+        GL_TRUE
+    );
+    
+    GLFWwindow* window
+        = glfwCreateWindow(800, 600, "Shaders", NULL, NULL);
+    
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+        glfwPollEvents();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
